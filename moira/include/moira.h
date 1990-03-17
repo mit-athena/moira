fdff5b98a8c26cb19b919af117ca53347b83ef71
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1990-03-17 16:02:42 +0000

    sms -> mr

--- moira/include/moira.h
+++ moira/include/moira.h
@@ -1,46 +1,46 @@
 /*
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/moira.h,v $
  *	$Author: mar $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/moira.h,v 1.13 1990-02-15 16:22:49 mar Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/moira.h,v 1.14 1990-03-17 16:02:42 mar Exp $
  *
  *	Copyright (C) 1987 by the Massachusetts Institute of Technology
  *
  */
 
-#ifndef _sms_h_
-#define _sms_h_
+#ifndef _moira_h_
+#define _moira_h_
 
 /* return values from queries (and error codes) */
 
 #include "sms_et.h"
-#define SMS_SUCCESS 0		/* Query was successful */
+#define MR_SUCCESS 0		/* Query was successful */
 
-#define SMS_VERSION_1 1		/* Version in use from 7/87 to 4/88 */
-#define SMS_VERSION_2 2		/* After 4/88, new query lib */
+#define MR_VERSION_1 1		/* Version in use from 7/87 to 4/88 */
+#define MR_VERSION_2 2		/* After 4/88, new query lib */
 
 /* return values for sms server calls, used by clients */
 
-#define SMS_CONT 0		/* Everything okay, continue sending values. */
-#define SMS_ABORT -1		/* Something went wrong don't send anymore
+#define MR_CONT 0		/* Everything okay, continue sending values. */
+#define MR_ABORT -1		/* Something went wrong don't send anymore
 				   values. */
 
 #define MOIRA_SNAME	"moira"	/* Kerberos service key to use */
 
 /* Protocol operations */
-#define SMS_NOOP 0
-#define SMS_AUTH 1
-#define SMS_SHUTDOWN 2
-#define SMS_QUERY 3
-#define SMS_ACCESS 4
-#define SMS_DO_UPDATE 5
-#define SMS_MOTD 6
-#define SMS_MAX_PROC 6
+#define MR_NOOP 0
+#define MR_AUTH 1
+#define MR_SHUTDOWN 2
+#define MR_QUERY 3
+#define MR_ACCESS 4
+#define MR_DO_UPDATE 5
+#define MR_MOTD 6
+#define MR_MAX_PROC 6
 
 /* values used in NFS physical flags */
-#define SMS_FS_STUDENT	0x0001
-#define	SMS_FS_FACULTY  0x0002
-#define SMS_FS_STAFF	0x0004
-#define SMS_FS_MISC	0x0008
+#define MR_FS_STUDENT	0x0001
+#define	MR_FS_FACULTY	0x0002
+#define MR_FS_STAFF	0x0004
+#define MR_FS_MISC	0x0008
 
 /* magic values to pass for list and user queries */
 #define UNIQUE_GID	"create unique GID"
@@ -68,17 +68,17 @@ struct hash {
 };
 
 #ifdef __STDC__
-int sms_connect();
-int sms_disconnect();
-int sms_auth();
-int sms_access(char *handle, int argc, char **argv);
-int sms_access_internal(int argc, char **argv);
-int sms_query(char *handle, int argc, char **argv,
+int mr_connect(char *server);
+int mr_disconnect();
+int mr_host(char *host, int size);
+int mr_auth(char *prog);
+int mr_access(char *handle, int argc, char **argv);
+int mr_access_internal(int argc, char **argv);
+int mr_query(char *handle, int argc, char **argv,
 	      int (*callback)(), char *callarg);
-int sms_query_internal(int argc, char **argv,
+int mr_query_internal(int argc, char **argv,
 		       int (*callback)(), char *callarg);
-int sms_noop();
-int sms_shutdown(char *reason);
+int mr_noop();
 struct save_queue *sq_create();
 struct hash *create_hash(int size);
 char *hash_lookup(struct hash *h, int key);
@@ -86,13 +86,13 @@ char *strsave(char *s);
 char *strtrim(char *s);
 char *canonicalize_hostname(char *s);
 #else !__STDC__
-int sms_connect();
-int sms_disconnect();
-int sms_auth();
-int sms_access();
-int sms_query();
-int sms_noop();
-int sms_shutdown();
+int mr_connect();
+int mr_disconnect();
+int mr_host();
+int mr_auth();
+int mr_access();
+int mr_query();
+int mr_noop();
 struct save_queue *sq_create();
 struct hash *create_hash();
 char *hash_lookup();
@@ -101,4 +101,4 @@ char *strtrim();
 char *canonicalize_hostname();
 #endif __STDC__
 
-#endif _sms_h_			/* Do Not Add Anything after this line. */
+#endif _moira_h_		/* Do Not Add Anything after this line. */

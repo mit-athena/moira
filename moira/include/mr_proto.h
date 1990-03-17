9d4363f7d27a27267d9fef804e8199c7b4a6faff
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1990-03-17 15:57:31 +0000

    sms -> mr

--- moira/include/mr_proto.h
+++ moira/include/mr_proto.h
@@ -1,37 +1,30 @@
 /*
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/mr_proto.h,v $
  *	$Author: mar $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/mr_proto.h,v 1.3 1989-10-06 19:09:59 mar Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/include/mr_proto.h,v 1.4 1990-03-17 15:57:31 mar Exp $
  *
- *	Copyright (C) 1987 by the Massachusetts Institute of Technology
+ *	Copyright (C) 1987, 1990 by the Massachusetts Institute of Technology
  *
- *	$Log: not supported by cvs2svn $
- * Revision 1.2  87/06/28  05:28:18  spook
- * Added domain name to 'sms'...
- * 
- * Revision 1.1  87/06/16  17:48:47  wesommer
- * Initial revision
- * 
  */
 
 #include <stdio.h>
 #include <gdb.h>
 #include <sms.h>
 
-typedef struct sms_params {
-	u_long sms_size;
-	u_long sms_version_no;
+typedef struct mr_params {
+	u_long mr_size;
+	u_long mr_version_no;
 	union {
 		u_long procno;	/* for call */
 		u_long status;	/* for reply */
 	} u;
-#define sms_procno u.procno
-#define sms_status u.status
-	int sms_argc;
-	char **sms_argv;
-	int *sms_argl;
-	char *sms_flattened;
-	int sms_state;
-} sms_params;
+#define mr_procno u.procno
+#define mr_status u.status
+	int mr_argc;
+	char **mr_argv;
+	int *mr_argl;
+	char *mr_flattened;
+	int mr_state;
+} mr_params;
 
-extern int sms_start_recv(), sms_start_send();
+extern int mr_start_recv(), mr_start_send();

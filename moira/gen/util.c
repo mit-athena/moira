9d8929c93f8940cbb0692c56e13dcee740dab4ec
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1990-08-17 18:32:02 +0000

    removed old Ingres date routines

--- moira/gen/util.c
+++ moira/gen/util.c
@@ -1,4 +1,4 @@
-/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/util.c,v 1.5 1990-03-19 19:07:45 mar Exp $
+/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/util.c,v 1.6 1990-08-17 18:32:02 mar Exp $
  *
  * Utility routines used by the MOIRA extraction programs.
  *
@@ -13,74 +13,6 @@
 #include <sys/time.h>
 #include <moira.h>
 #include <moira_site.h>
-
-
-/* ingres_date_and_time: passed a unix time_t, returns a string that ingres
- * can parse to obtain that time.
- */
-
-static char *month_name[] = {
-    "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct",
-    "nov", "dec"
-  };
-
-
-char *ingres_date(), *ingres_time();
-
-char *ingres_date_and_time(l)
-long l;
-{
-	char *ans = NULL, *date, *time;
-      
-	if ((date = ingres_date(l)) && (time = ingres_time(l))) {
-		char buf[BUFSIZ];
-		sprintf(buf, "%s %s", date, time);
-		ans = strsave(buf);
-	}
-	if (date)
-		free(date);
-	if (time)
-		free(time);
-	return ans;
-}
-
-char *ingres_time(t)
-	long t;
-{
-	struct tm *tm;
-
-	if (t == (long) 0)
-		(void) time(&t);
-
-	if ((tm = localtime(&t)) == (struct tm *) NULL) {
-		return NULL;
-	} else {
-		char buf[BUFSIZ];
-
-		sprintf(buf, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min,
-			tm->tm_sec);
-		return strsave(buf);
-	}
-}
-
-char *ingres_date(t)
-	long t;
-{
-	struct tm *tm;
-
-	if (t == (long) 0)
-		(void) time(&t);
-
-	if ((tm = localtime(&t)) == (struct tm *) NULL) {
-		return NULL;
-	} else {
-		char buf[BUFSIZ];
-
-		sprintf(buf, "%02d-%3.3s-%04d", tm->tm_mday,
-			month_name[tm->tm_mon], 1900 + tm->tm_year);
-		return strsave(buf);
-	}
-}
 
 
 fix_file(targetfile)
@@ -124,4 +56,5 @@ register char *s;
       *last = '\0';
     else
       *(++last) = '\0';
+    return(s);
 }

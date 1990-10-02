3801cfe0cef58c24a8e37960a34733ee758736df
kostas <kostas@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1990-10-02 13:31:17 +0000

    *** empty log message ***

--- moira/server/sql_defs.h
+++ moira/server/sql_defs.h
@@ -1,13 +1,13 @@
-/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/sql_defs.h,v 1.2 1990-10-02 09:25:53 kostas Exp $
+/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/sql_defs.h,v 1.3 1990-10-02 13:31:17 kostas Exp $
  *
  * Structures and constants used by SQL related code
  */
 
 /* SQL character string constants .. */
-#define SQL_STR         256
-#define SQL_LONG_STR    512
-#define SQL_MAX_STR_LEN 512
+#define SQL_STR           256
+#define SQL_LONG_STR     1024
+#define SQL_MAX_STR_LEN 16383
 
 /* SQL table related constants */
-#define SQL_MAX_COLUMS  128
+#define SQL_MAX_COLUMS    128
 

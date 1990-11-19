04bbb6675bd273574e2a9f5dffee5514be9f6d5f
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1990-11-19 12:18:53 +0000

    don't conditionalize on hostname; append local aliases file

--- moira/gen/aliases.sh
+++ moira/gen/aliases.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 MR_MKCRED=47836474
-if [ `hostname`x != MITx ]; then
-	/bin/diff /usr/lib/aliases /usr/lib/aliases.new > /tmp/aliasdiff
-	fi
+cat /usr/lib/local-aliases >> /usr/lib/aliases.new
+/bin/diff /usr/lib/aliases /usr/lib/aliases.new > /tmp/aliasdiff
 /bin/cp /dev/null /usr/lib/aliases.new.dir
 /bin/cp /dev/null /usr/lib/aliases.new.pag
 /usr/lib/sendmail -bi -oA/usr/lib/aliases.new
 if [ $? != 0 ]; then
 	exit $MR_MKCRED
 	fi
-if [ `hostname`x = MITx ]; then
-	kill `/bin/ps ax | /bin/grep "accepting connections" | /bin/grep -v grep | /bin/awk '{print $1}'`
-	/bin/mv /usr/lib/aliases /usr/lib/aliases.old
-	/bin/mv /usr/lib/aliases.dir /usr/lib/aliases.old.dir
-	/bin/mv /usr/lib/aliases.pag /usr/lib/aliases.old.pag
-	/bin/mv /usr/lib/aliases.new /usr/lib/aliases
-	/bin/mv /usr/lib/aliases.new.dir /usr/lib/aliases.dir
-	/bin/mv /usr/lib/aliases.new.pag /usr/lib/aliases.pag
-	/usr/lib/sendmail -bd -q30m
-	fi
+kill `/bin/ps ax | /bin/grep "accepting connections" | /bin/grep -v grep | /bin/awk '{print $1}'`
+/bin/mv /usr/lib/aliases /usr/lib/aliases.old
+/bin/mv /usr/lib/aliases.dir /usr/lib/aliases.old.dir
+/bin/mv /usr/lib/aliases.pag /usr/lib/aliases.old.pag
+/bin/mv /usr/lib/aliases.new /usr/lib/aliases
+/bin/mv /usr/lib/aliases.new.dir /usr/lib/aliases.dir
+/bin/mv /usr/lib/aliases.new.pag /usr/lib/aliases.pag
+/usr/lib/sendmail -bd -q30m
+
 /bin/rm -f $0
 exit 0
 
-# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/aliases.sh,v 1.4 1990-03-19 19:05:53 mar Exp $
+# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/aliases.sh,v 1.5 1990-11-19 12:18:53 mar Exp $

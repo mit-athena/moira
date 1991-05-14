2a5a86788e2da6b1d37660447a6dc485c12a783c
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1991-05-14 18:02:33 +0000

    don't screw with unterminated strings

--- moira/clients/userreg/files.h
+++ moira/clients/userreg/files.h
@@ -2,29 +2,14 @@
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v $
  *	$Author: mar $
  *	$Locker:  $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v 1.5 1990-03-13 12:59:11 mar Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v 1.6 1991-05-14 18:02:33 mar Exp $
  */
 
-#ifndef __SABER__
-#define TEXT "/mit/register
-
-#define WELCOME TEXT/welcome"	/* welcome message */
-#define NETWORK_DOWN TEXT/network_down"
-#define USERNAME_BLURB TEXT/username_blurb"
-#define PASSWORD_BLURB TEXT/password_blurb"
-#define DELETED_ACCT TEXT/deleted_acct"
-#define NOT_ALLOWED TEXT/not_allowed"
-#define IMPROPER_LOGIN TEXT/improper_reg"
-#define OFFER_ENROLL TEXT/offer_enroll"
-#else
-#define TEXT "/mit/register"
-
-#define WELCOME TEXT "/welcome"	/* welcome message */
-#define NETWORK_DOWN TEXT "/network_down"
-#define USERNAME_BLURB TEXT "/username_blurb"
-#define PASSWORD_BLURB TEXT "/password_blurb"
-#define DELETED_ACCT TEXT "/deleted_acct"
-#define NOT_ALLOWED TEXT "/not_allowed"
-#define IMPROPER_LOGIN TEXT "/improper_reg"
-#define OFFER_ENROLL TEXT "/offer_enroll"
-#endif __SABER__
+#define WELCOME		"/mit/register/welcome"
+#define NETWORK_DOWN	"/mit/register/network_down"
+#define USERNAME_BLURB	"/mit/register/username_blurb"
+#define PASSWORD_BLURB	"/mit/register/password_blurb"
+#define DELETED_ACCT	"/mit/register/deleted_acct"
+#define NOT_ALLOWED	"/mit/register/not_allowed"
+#define IMPROPER_LOGIN	"/mit/register/improper_reg"
+#define OFFER_ENROLL	"/mit/register/offer_enroll"

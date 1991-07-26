a7cdc88211e19c1cab55c878a0da24ddb2fe90d7
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1991-07-26 14:52:08 +0000

    added some new files

--- moira/clients/userreg/files.h
+++ moira/clients/userreg/files.h
@@ -2,14 +2,23 @@
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v $
  *	$Author: mar $
  *	$Locker:  $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v 1.6 1991-05-14 18:02:33 mar Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/userreg/files.h,v 1.7 1991-07-26 14:52:08 mar Exp $
  */
 
 #define WELCOME		"/mit/register/welcome"
-#define NETWORK_DOWN	"/mit/register/network_down"
 #define USERNAME_BLURB	"/mit/register/username_blurb"
+#define USERNAME_BLURB2	"/mit/register/username_blurb2"
 #define PASSWORD_BLURB	"/mit/register/password_blurb"
+#define PASSWORD_BLURB2	"/mit/register/password_blurb2"
+#define FINISHED	"/mit/register/finished"
+
+#define NETWORK_DOWN	"/mit/register/network_down"
 #define DELETED_ACCT	"/mit/register/deleted_acct"
 #define NOT_ALLOWED	"/mit/register/not_allowed"
 #define IMPROPER_LOGIN	"/mit/register/improper_reg"
 #define OFFER_ENROLL	"/mit/register/offer_enroll"
+#define FINISHEDE	"/mit/register/finishede"
+
+#define NAMESPACE_PROG	"/mit/register/@sys/namespace"
+#define DISABLE_FILE	"/mit/register/disabled.times"
+#define LOGIN_INFO	"/mit/register/login.info"

f51c385e32a15a1332c29fdb5d4fcb7cdc2b4032
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1992-10-05 11:40:08 +0000

    changed path from /u1/usr/lib/... to /usr/lib/...

--- moira/gen/mailhub.sh
+++ moira/gen/mailhub.sh
@@ -3,24 +3,24 @@ PATH=/bin:/usr/ucb:/usr/bin
 
 MR_MKCRED=47836474
 
-cat /u1/usr/lib/local-aliases >> /u1/usr/lib/aliases.new
-cp /dev/null /u1/usr/lib/aliases.new.dir
-cp /dev/null /u1/usr/lib/aliases.new.pag
-/usr/lib/sendmail -bi -oA/u1/usr/lib/aliases.new
+cat /usr/lib/local-aliases >> /usr/lib/aliases.new
+cp /dev/null /usr/lib/aliases.new.dir
+cp /dev/null /usr/lib/aliases.new.pag
+/usr/lib/sendmail -bi -oA/usr/lib/aliases.new
 if [ $? != 0 ]; then
 	exit $MR_MKCRED
 	fi
 
 kill `ps ax | grep "accepting connections" | grep -v grep | awk '{print $1}'`
-mv /u1/usr/lib/aliases /u1/usr/lib/aliases.old
-mv /u1/usr/lib/aliases.dir /u1/usr/lib/aliases.old.dir
-mv /u1/usr/lib/aliases.pag /u1/usr/lib/aliases.old.pag
-mv /u1/usr/lib/aliases.new /u1/usr/lib/aliases
-mv /u1/usr/lib/aliases.new.dir /u1/usr/lib/aliases.dir
-mv /u1/usr/lib/aliases.new.pag /u1/usr/lib/aliases.pag
+mv /usr/lib/aliases /usr/lib/aliases.old
+mv /usr/lib/aliases.dir /usr/lib/aliases.old.dir
+mv /usr/lib/aliases.pag /usr/lib/aliases.old.pag
+mv /usr/lib/aliases.new /usr/lib/aliases
+mv /usr/lib/aliases.new.dir /usr/lib/aliases.dir
+mv /usr/lib/aliases.new.pag /usr/lib/aliases.pag
 /usr/lib/sendmail -bd -q30m
 
 rm -f $0
 exit 0
 
-# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/mailhub.sh,v 1.5 1992-06-22 11:15:23 mar Exp $
+# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/mailhub.sh,v 1.6 1992-10-05 11:40:08 mar Exp $

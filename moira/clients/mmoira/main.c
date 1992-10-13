bf2543d39f46a2508a6e75590c808c2f0c85c1d8
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1992-10-13 11:15:25 +0000

    install keyboard accelerators; parse server off command line; be intelligent  about kerberos errors

--- moira/clients/mmoira/main.c
+++ moira/clients/mmoira/main.c
@@ -1,4 +1,4 @@
-/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/main.c,v 1.3 1991-06-05 12:26:30 mar Exp $
+/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/main.c,v 1.4 1992-10-13 11:15:25 mar Exp $
  *
  *  	Copyright 1991 by the Massachusetts Institute of Technology.
  *
@@ -8,16 +8,28 @@
 
 #include	<mit-copyright.h>
 #include	<stdio.h>
+#include	<pwd.h>
 #include	<moira.h>
 #include	<Xm/PushB.h>
 #include	<Xm/BulletinB.h>
+#include	<Xm/RowColumn.h>
+#include	<Xm/RowColumnP.h>
 #include	"mmoira.h"
 
 extern MenuItem MenuRoot;
 
 Widget	CreateMenu(), CreateForm();
 Widget	BuildMenuTree();
-void popup_error_hook(), mr_x_input();
+void popup_error_hook(), mr_x_input(), EnterPressed(), CancelForm();
+void ExecuteForm(), DoHelp();
+
+XtActionsRec actions[] = {
+    { "next-or-do-it", EnterPressed },
+    { "cancel-form", CancelForm },
+    { "execute-form", ExecuteForm },
+    { "help", DoHelp }
+};
+
 
 Widget  toplevel;
 char *user, *program_name, *moira_server;
@@ -29,19 +41,22 @@ char *argv[];
 	Widget	button, bboard, menuwidget;
 	char *motd;
 	int	n, status;
+	char *getlogin();
 
 	if ((user = getlogin()) == NULL)
 	  user = getpwuid((int) getuid())->pw_name;
-	user = (user && strlen(user)) ? Strsave(user) : "";
+	user = (user && strlen(user)) ? strsave(user) : "";
 
 	if ((program_name = rindex(argv[0], '/')) == NULL)
 	  program_name = argv[0];
 	else
 	  program_name++;
-	program_name = Strsave(program_name);
+	program_name = strsave(program_name);
 
-	/* Need to parse command line argument here */
 	moira_server = "";
+	for (n = 1; n < argc - 1; n++)
+	  if (!strcmp(argv[n], "-db"))
+	    moira_server = argv[n + 1];
 
 	status = mr_connect(moira_server);
 	if (status) {
@@ -66,16 +81,15 @@ char *argv[];
 	    com_err(program_name, status, "\nPress [RETURN] to continue");
 	    gets(buf);
 	} else if (status) {
-	    if (status >= ERROR_TABLE_BASE_krb &&
-		status <= ERROR_TABLE_BASE_krb + 256)
-	      com_err(program_name, status, "; authorization failed - please run kinit");
-	    else
-	      com_err(program_name, status, "; authorization failed");
+	    com_err(program_name, status, "; authorization failed - may need to run kinit");
 	    exit(1);
 	}
 
 	toplevel = XtInitialize("toplevel", "Moira", NULL, 0, 
 				&argc, argv);
+
+	XtAppAddActions(XtWidgetToApplicationContext(toplevel),
+			actions, XtNumber(actions));
 
 	bboard = XtCreateManagedWidget(	"bboard",
 				       xmBulletinBoardWidgetClass,
@@ -129,13 +143,37 @@ caddr_t data;
 }
 
 
+static char form_override_table[] =
+    "None<Key>Return:	next-or-do-it()\n\
+     Ctrl<Key>C:	cancel-form()\n\
+     Shift<Key>Return:	execute-form()\n\
+     Meta<Key>?:	help()";
+
+
 DisplayForm(spec)
 EntryForm	*spec;
 {
 	Widget	w;
+	int i, j;
+	static XtTranslations trans = NULL;
+
+	if (trans == NULL)
+	  trans = XtParseTranslationTable(form_override_table);
 
 	w = CreateForm(toplevel, spec);
 	XtManageChild(w);
+	for (i = 0; spec->inputlines[i]; i++) {
+	    XtOverrideTranslations(spec->inputlines[i]->mywidget, trans);
+	    if (spec->inputlines[i]->type == FT_KEYWORD) {
+		XmRowColumnWidget rc;
+
+		rc = (XmRowColumnWidget) spec->inputlines[i]->mywidget;
+		for (j = 0; j < rc->composite.num_children; j++)
+		  XtOverrideTranslations(rc->composite.children[j], trans);
+	    }
+	}
+	/* set the focus to the first line of the form */
+	_XmGrabTheFocus(spec->inputlines[0]->mywidget, NULL);
 }
 
 

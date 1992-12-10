765fe54e30476ac6680540620370d9bf60810be5
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1992-12-10 11:00:53 +0000

    remove unused code; redo parsing of menus; add missing code

--- moira/clients/mmoira/tty.c
+++ moira/clients/mmoira/tty.c
@@ -1,4 +1,4 @@
-/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/tty.c,v 1.1 1992-11-24 16:57:30 mar Exp $
+/* $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/tty.c,v 1.2 1992-12-10 11:00:53 mar Exp $
  *
  *  	Copyright 1992 by the Massachusetts Institute of Technology.
  *
@@ -15,274 +15,57 @@
 #include	<ctype.h>
 #include	<strings.h>
 #include	<X11/Intrinsic.h>
+#include	<moira.h>
 #include	"mmoira.h"
+#include	"parser.h"
 
-static char rcsid[] = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/tty.c,v 1.1 1992-11-24 16:57:30 mar Exp $";
+static char rcsid[] = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/mmoira/tty.c,v 1.2 1992-12-10 11:00:53 mar Exp $";
 
 
-struct TtyMenu {
-    char line[48];
-    MenuItem *menu;
-} *TtyCommands = NULL;
+struct parse_node *TtyCommands = NULL, *TtyRoot = NULL;
 
 extern int NumMenus;
 extern MenuItem MenuRoot;
 char prompt[] = "moira> ";
 
-static menucmp(a, b)
-struct TtyMenu *a, *b;
-{
-    return(strcmp(a->line, b->line));
-}
+static struct sgttyb otty, ntty;
 
 
 TtyMainLoop()
 {
     char buf[1024], cbuf[2], c, *p;
     int arg, i, done;
-    struct sgttyb tty, ntty;
 
-    if (TtyCommands == NULL) {
-	parse_menus();
-    }
+    if (TtyCommands == NULL)
+      parse_menus();
+
+#ifdef DEBUG
+    print_parse_tree(TtyRoot, 0);
+    fflush(stdout);
+    sleep(10);
+#endif /* DEBUG */
 
     ioctl(0, TIOCFLUSH, &arg);
-    ioctl(0, TIOCGETP, &tty);
-    ntty = tty;
+    ioctl(0, TIOCGETP, &otty);
+    ntty = otty;
     ntty.sg_flags |= RAW;
     ntty.sg_flags &= ~ECHO;
-    ioctl(0, TIOCSETP, &ntty);
+    raw_mode();
 
     while (1) {
-	parser(prompt, &TtyCommands[0]);
-    }{
-	write(1, prompt, strlen(prompt));
-	p = &buf[0];
-	buf[0] = '\0';
-	for (done = 0; !done;) {
-	    i = read(0, cbuf, 1);
-	    c = cbuf[0];
-	    cbuf[1] = '\0';
-	    switch (c) {
-		/* Backspace, Delete */
-	    case 0177:
-	    case '\b':
-		if (p > &buf[0]) {
-		    if (p[-1] == '\t') {
-			*(--p) = '\0';
-			write(1, "\r", 1);
-			write(1, prompt, strlen(prompt));
-			write(1, buf, strlen(buf));
-		    } else {
-			write(1, "\b \b", 3);
-			*(--p) = '\0';
-		    }
-		}
-		break;
-		/* Erase Word */
-	    case 'W' - '@':
-		if (p > &buf[0])
-		  p--;
-		while (p > &buf[0] && isspace(*p)) {
-		    write(1, "\b \b", 3);
-		    p--;
-		}
-		while (p >= &buf[0] && !isspace(*p)) {
-		    write(1, "\b \b", 3);
-		    p--;
-		}
-		if (p > &buf[0])
-		  p++;
-		else
-		  p = &buf[0];
-		*p = 0;
-		break;
-		/* Suspend */
-	    case 'Z' - '@':
-		ioctl(0, TIOCSETP, &tty);
-		kill(getpid(), SIGSTOP);
-		ioctl(0, TIOCSETP, &ntty);
-		write(1, prompt, strlen(prompt));
-		write(1, buf, strlen(buf));
-		break;
-		/* Exit */
-	    case 'C' - '@':
-		write(1, "\r\n", 2);
-		ioctl(0, TIOCSETP, &tty);
-		exit(0);
-		break;
-		/* Clear Line */
-	    case 'U' - '@':
-		while (p > &buf[0]) {
-		    write(1, "\b \b", 3);
-		    *(--p) = '\0';
-		}
-		write(1, "\r", 1);
-		write(1, prompt, strlen(prompt));
-		break;
-		/* Redraw Line */
-	    case 'L' - '@':
-	    case 'R' - '@':
-		write(1, "XXX\r\n", 5);
-		write(1, prompt, strlen(prompt));
-		write(1, buf, p - &buf[0]);
-		break;
-		/* Complete field */
-	    case '\t':
-	    case '[' - '@':
-		do_complete(buf);
-		break;
-		/* Help */
-	    case '?':
-		write(1, "?  ", 3);
-		do_help(buf);
-		write(1, prompt, strlen(prompt));
-		write(1, buf, strlen(buf));
-		break;
-		/* Execute Command */
-	    case '\n':
-	    case '\r':
-		done = 1;
-		write(1, "\r\n", 2);
-		execute_command(buf);
-		break;
-		/* Quote character */
-	    case 'Q'  - '@':
-	    case 'V' - '@':
-		i = read(0, cbuf, 1);
-		c = cbuf[0];
-		/* Insert character */
-	    default:
-		write(1, cbuf, 1);
-		*p++ = c;
-		*p = '\0';
-	    }
-	}
+	parser(prompt, TtyRoot);
     }
 }
 
-
-static char *NextWord(s)
-char *s;
+cooked_mode()
 {
-    while (*s && !isspace(*s)) s++;
-    while (*s && isspace(*s)) s++;
-    return(s);
+    ioctl(0, TIOCSETP, &otty);
 }
 
-static do_match(line, word)
-char *line;
-int word;
+raw_mode()
 {
-    int i, j, k, match, len;
-    char *p, *wrd, *linep;
-
-    linep = NthWord(line, word);
-    if (linep == NULL) {
-	return(NULL);
-    }
-    p = index(linep, ' ');
-    if (p)
-      len = p - linep;
-    else
-      len = strlen(linep);
-    match = -1;
-
-    for (i = 0; TtyCommands[i].menu; i++) {
-	j = strncmp(NthWord(TtyCommands[i].line, word), linep, len);
-	if (j == 0 && match == -1) match = i;
-	if (j > 0) {
-	    if (match == -1) {
-		return(NULL);
-	    }
-	    if (match == i - 1) {
-		return(i);
-	    }
-	    if (NumWords(line) > 1) {
-		word = NextWord(line);
-		p = index(word, ' ');
-		if (p)
-		  len = p - word;
-		else
-		  len = strlen(word);
-		for (k = match; k < i; i++) {
-		}
-	    }
-	    for (j = match; j < i; j++) {
-		write(1, "\r\n  ", 4);
-		write(1, TtyCommands[j].line, strlen(TtyCommands[j].line));
-	    }
-	    write(1, "\r\n", 2);
-	    return;
-	}
-    }
-    printf("How'd we get here? j=%d, i=%d\r\n", j, i); fflush(stdout);
+    ioctl(0, TIOCSETP, &ntty);
 }
-
-do_help(line)
-char *line;
-{
-    int i, j, k, match, len;
-    char *p, *word;
-
-    p = index(line, ' ');
-    if (p)
-      len = p - line;
-    else
-      len = strlen(line);
-    match = -1;
-
-    printf("Help(%s) len %d\r\n", line, len); fflush(stdout);
-
-    for (i = 0; TtyCommands[i].menu; i++) {
-	printf("strncmp(%s, %s, %d)\r\n", TtyCommands[i].line, line, len);
-	fflush(stdout);
-	j = strncmp(TtyCommands[i].line, line, len);
-	if (j == 0 && match == -1) match = i;
-	if (j > 0) {
-	    if (match == -1) {
-		write(1, "Nothing matches\r\n", 17);
-		printf("Comparing %s\r\n", TtyCommands[i].line); fflush(stdout);
-		return;
-	    }
-	    if (match == i - 1) {
-		write(1, "Command, only one matches:\r\n  ", 30);
-		write(1, TtyCommands[i-1].line, strlen(TtyCommands[i-1].line));
-		write(1, "\r\n", 2);
-		return;
-	    }
-	    if (NumWords(line) > 1) {
-		word = NextWord(line);
-		p = index(word, ' ');
-		if (p)
-		  len = p - word;
-		else
-		  len = strlen(word);
-		for (k = match; k < i; i++) {
-		}
-	    }
-	    write(1, "Command, one of:", 16);
-	    for (j = match; j < i; j++) {
-		write(1, "\r\n  ", 4);
-		write(1, TtyCommands[j].line, strlen(TtyCommands[j].line));
-	    }
-	    write(1, "\r\n", 2);
-	    return;
-	}
-    }
-    printf("How'd we get here? j=%d, i=%d\r\n", j, i); fflush(stdout);
-}
-
-do_complete(line)
-char *line;
-{
-}
-
-execute_command(line)
-char *line;
-{
-}
-
 
 static NumWords(s)
 char *s;
@@ -295,73 +78,88 @@ char *s;
 }
 
 
-static parse_menus()
+parse_menus()
 {
     int arg;
+    struct parse_node *p;
 
-    TtyCommands = (struct TtyMenu *)malloc(sizeof(struct TtyMenu) *
-					   NumMenus * 3);
+    TtyCommands = (struct parse_node *)malloc(sizeof(struct parse_node) *
+					      NumMenus * 3);
+    bzero(TtyCommands, sizeof(struct parse_node) * NumMenus * 3);
     arg = 0;
     parse_menu_recursive(&MenuRoot, "", &arg);
+    TtyCommands[arg].p_word = "help";
+    TtyCommands[arg].p_next = TtyRoot;
+    for (p = TtyRoot; p; p = p->p_peer)
+      if (strcmp(p->p_peer->p_word, "help") > 0)
+	break;
+    TtyCommands[arg].p_peer = p->p_peer->p_peer;
+    p->p_peer = &TtyCommands[arg];
+    arg++;
 }
 
 
-static parse_menu_recursive(m, parent, i)
+parse_menu_recursive(m, parent, i)
 MenuItem *m;
 char *parent;
 int *i;
 {
-    char buf[64], cmd[64], *word;
+    char buf[64], cmd[64], *word, *s;
     struct parse_node *p, **prev;
-    int j;
+    int j, len;
 
     if (m->submenu == NULL) {
 	if (!strcmp(parent, "file") ||
 	    !strcmp(parent, "misc"))
 	  parent = "";
-	if (NumWords(m->label) > 2)
+	if (NumWords(m->label) > 2 || !*parent)
 	  strcpy(cmd, m->label);
 	else
 	  sprintf(cmd, "%s %s", m->label, parent);
 	/* insert command into parse tree */
-	p = &TtyCommands[0];
-	prev = (struct parse_node **) NULL;
-	for (word = cmd; word != (char  *)1; word = index(word, ' ') + 1) {
+	p = TtyRoot;
+	prev = (struct parse_node **) &TtyRoot;
+	for (word = cmd; word; word = index(word, ' ')) {
+	    if (*word == ' ')
+	      word++;
 	    s = index(word, ' ');
 	    if (s)
 	      len = s - word;
 	    else
 	      len = strlen(word);
-	    for (; !p || p->p_peer; p = p->p_peer) {
-		if (!p || (j = strncmp(p->data, word, len)) < 0) {
-		    TtyCommands[*i].p_type = KEYWORD;
+	    for (;;) {
+		if (!p || !p->p_word ||
+		    (j = strncmp(p->p_word, word, len)) > 0) {
 		    strcpy(buf, word);
 		    buf[len] = 0;
+#ifdef DEBUG
+		    printf("word: %s\n", buf);
+#endif
 		    TtyCommands[*i].p_word = strsave(buf);
-		    TtyCommands[*i].p_noise = NULL;
-		    TtyCommands[*i].p_param = m;
-		    TtyCommands[*i].p_help = NULL;
-		    TtyCommands[*i].p_ret = NONE;
+		    if (len == strlen(word))
+		      TtyCommands[*i].p_menu = m;
 		    TtyCommands[*i].p_peer = p;
 		    if (prev)
-		      prev->p_peer = &TtyCommands[*i];
+		      *prev = &TtyCommands[*i];
 		    TtyCommands[*i].p_next = (struct parse_node *) NULL;
 		    TtyCommands[*i].p_link = (struct parse_node *) NULL;
-		    i++;
+		    prev = &(TtyCommands[*i].p_next);
+		    p = (struct parse_node *) NULL;
+		    *i = *i + 1;
 		    break;
 		} else if (j == 0) {
 		    prev = &(p->p_next);
 		    if (p->p_next)
-		      p = p->next;
+		      p = p->p_next;
 		    else
 		      p = (struct parse_node *) NULL;
 		    break;
 		} else {
 		    prev = &(p->p_peer);
+		    p = p->p_peer;
 		}
 	    }
 	}
-	*i = *i + 1;
 	return;
     }
     if (!strcmp(m->label, "?help")) return;
@@ -380,4 +178,208 @@ int *i;
 TtyForm(f)
 EntryForm *f;
 {
+    int i, j, done, best, choice;
+    char buf[512], *k;
+    UserPrompt *p;
+
+    printf("%s\r\n", f->instructions);
+    choice = 0;
+    for (i = 0, p = f->inputlines[i]; p = f->inputlines[i]; i++)
+      if (p->choice) {
+	  choice = 1;
+	  break;
+      }
+    if (choice) {
+	j = 0;
+	*buf = 0;
+	for (i = 0, p = f->inputlines[i]; p = f->inputlines[i]; i++) {
+	    if (p->choice) {
+		printf("  %s\r\n", buf);
+		sprintf(buf, "%d: %s", ++j, p->prompt);
+	    } else if (*buf == 0) {
+		sprintf(buf, "%d: %s", ++j, p->prompt);
+	    } else {
+		strcat(buf, "/");
+		strcat(buf, p->prompt);
+	    }
+	    if (buf[strlen(buf)-1] == ' ') buf[strlen(buf)-1] = 0;
+	}
+	printf("  %s\r\nSelect by [1]: ", buf);
+	if (mgets(buf, sizeof(buf)))
+	  return;
+	if (buf[strlen(buf)-1] == '\n') buf[strlen(buf)-1] = 0;
+	if (strlen(buf) == 0)
+	  choice = 1;
+	else
+	  choice = atoi(buf);
+    } else choice = -1;
+    for (i = 0, p = f->inputlines[i]; p = f->inputlines[i]; i++) {
+	if (choice > 0 && p->choice) choice--;
+	if (choice != -1 && choice != 1) continue;
+	if (p->insensitive == True) continue;
+	switch (p->type) {
+	case FT_BOOLEAN:
+	    done = 0;
+	    while (!done) {
+		done = 1;
+		printf("%s(T/F): ", p->prompt);
+		fflush(stdout);
+		if (mgets(buf, sizeof(buf)))
+		  return;
+		if (buf[0] == 'T' || buf[0] == 't' ||
+		    buf[0] == 'Y' || buf[0] == 'y')
+		  p->returnvalue.booleanvalue = True;
+		else if (buf[0] == 'F' || buf[0] == 'f' ||
+			 buf[0] == 'N' || buf[0] == 'n')
+		  p->returnvalue.booleanvalue = False;
+		else {
+		    done = 0;
+		    printf("Please answer True or False\r\n");
+		}
+	    }
+	    break;
+	case FT_STRING:
+	    if (*StringValue(f, i))
+	      printf("%s[%s]: ", p->prompt, StringValue(f, i));
+	    else
+	      printf("%s: ", p->prompt);
+	    fflush(stdout);
+	    if (mgets(buf, sizeof(buf)))
+	      return;
+	    if (buf[strlen(buf)-1] == '\n') buf[strlen(buf)-1] = 0;
+	    if (strlen(buf) != 0)
+	      StoreField(f, i, buf);
+	    if (!strcmp(buf, "\"\""))
+	      StoreField(f, i, "");
+	    break;
+	case FT_KEYWORD:
+	    k = index(p->prompt, '|');
+	    if (k) *k = 0;
+	    done = 0;
+	    while (done != 1) {
+		if (*StringValue(f, i))
+		  printf("%s[%s]: ", p->prompt, StringValue(f, i));
+		else
+		  printf("%s: ", p->prompt);
+		fflush(stdout);
+		if (mgets(buf, sizeof(buf)))
+		  return;
+		if (buf[strlen(buf)-1] == '\n') buf[strlen(buf)-1] = 0;
+		if (strlen(buf) == 0)
+		  strcpy(buf, StringValue(f, i));
+		done = 0;
+		for (j = 0; p->keywords[j]; j++)
+		  if (!strncasecmp(p->keywords[j], buf, strlen(buf)))
+		    done++, best = j;
+		if (done != 1) {
+		    printf("You must enter one of these keywords:\r\n");
+		    for (j = 0; p->keywords[j]; j++) {
+			printf("  %s\r\n", p->keywords[j]);
+		    }
+		} else
+		  StoreField(f, i, p->keywords[best]);
+	    }
+	    break;
+	}
+    }
+    process_form(f, TRUE);
+}
+
+
+int mgets(line, linelen)
+char *line;
+int linelen;
+{
+    char c, *p;
+
+    raw_mode();
+    p = &line[0];
+    *p = 0;
+    for (c = (getchar() & 0x7f); 1; c = (getchar() & 0x7f)) {
+	if (c == 0) continue;
+	switch (c) {
+	case 127:
+	case '\b':
+	    if (p == &line[0]) {
+		putchar(7);
+		break;
+	    }
+	    *(--p) = 0;
+	    write(1, "\b \b", 3);
+	    break;
+	case 'C' - '@':
+	case 'G' - '@':
+	    cooked_mode();
+	    return(-1);
+	case 'Q' - '@':
+	case 'V' - '@':
+	    putchar('\\');
+	    c = getchar();
+	    if (c < ' ')
+	      printf("\b^%c", c + '@');
+	    else
+	      printf("\b%c", c);
+	    fflush(stdout);
+	    *p++ = c;
+	    break;
+	case 'W' - '@':
+	    if (p > &line[0])
+	      p--;
+	    while ((p >= &line[0]) && isspace(*p)) {
+		write(1, "\b \b", 3);
+		p--;
+	    }
+	    while ((p >= &line[0]) && !isspace(*p)) {
+		write(1, "\b \b", 3);
+		p--;
+	    }
+	    if (p > &line[0]) {
+		p++;
+	    } else {
+		p = &line[0];
+	    }
+	    *p = 0;
+	    break;
+	case 'Z' - '@':
+	    printf("\r\n");
+	    kill(getpid(), SIGSTOP);
+	    /* when continued, fall through to */
+	case 'R' - '@':
+	    *p = 0;
+	    printf("\r\n%s", line);
+	    fflush(stdout);
+	    break;
+	case 'U' - '@':
+	    while (p-- > &line[0])
+	      write(1, "\b \b", 3);
+	    *(++p) = 0;
+	    fflush(stdout);
+	    break;
+	case '\n':
+	case '\r':
+	    write(1, "\r\n", 2);
+	    cooked_mode();
+	    return(0);
+	    break;
+	default:
+	    putchar(c);
+	    *p++ = c;
+	    *p = 0;
+	}
+    }
+}
+
+
+print_parse_tree(n, l)
+struct parse_node *n;
+int l;
+{
+    int i;
+
+    for (i = l; i > 0; i--) putchar(' ');
+    printf("%s%c\n", n->p_word, n->p_menu ? '*' : ' ');
+    if (n->p_next)
+      print_parse_tree(n->p_next, l+1);
+    if (n->p_peer)
+      return(print_parse_tree(n->p_peer, l));
 }

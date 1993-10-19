91a68a1ffefa961acf65615d967ade72aafd73ea
mar <mar@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1993-10-19 12:16:30 +0000

    compile on AIX 3.2

--- moira/update/exec_002.c
+++ moira/update/exec_002.c
@@ -1,20 +1,26 @@
 /*
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/exec_002.c,v $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/exec_002.c,v 1.12 1993-05-24 15:11:55 mar Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/exec_002.c,v 1.13 1993-10-19 12:16:30 mar Exp $
  */
 /*  (c) Copyright 1988 by the Massachusetts Institute of Technology. */
 /*  For copying and distribution information, please see the file */
 /*  <mit-copyright.h>. */
 
 #ifndef lint
-static char *rcsid_exec_002_c = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/exec_002.c,v 1.12 1993-05-24 15:11:55 mar Exp $";
+static char *rcsid_exec_002_c = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/exec_002.c,v 1.13 1993-10-19 12:16:30 mar Exp $";
 #endif	lint
 
 #include <mit-copyright.h>
 #include <stdio.h>
 #include <errno.h>
 #include <sys/types.h>
+#ifdef _AIX
+#undef _BSD
+#endif
 #include <sys/wait.h>
+#ifdef _AIX
+#define _BSD 44
+#endif
 #include <signal.h>
 #include <gdb.h>
 #include <moira.h>

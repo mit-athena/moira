179925920da4ef5f2a39f77710ec8ba63bea353a
probe <probe@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1994-10-18 13:42:39 +0000

    Add the pathname to the zwrite binary

--- moira/incremental/afs_utils.pl
+++ moira/incremental/afs_utils.pl
@@ -1,9 +1,10 @@
-# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/incremental/afs_utils.pl,v 1.11 1994-10-17 15:18:01 probe Exp $
+# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/incremental/afs_utils.pl,v 1.12 1994-10-18 13:42:39 probe Exp $
 
 $afsbin="/moira/bin";
 $vos="$afsbin/vos";
 $pts="$afsbin/pts";
 $fs="$afsbin/fs";
+$zwrite="/usr/athena/bin/zwrite"
 
 $afs_data="/moira/afs/afs_data";
 $afs_save="$afs_data.tmp";

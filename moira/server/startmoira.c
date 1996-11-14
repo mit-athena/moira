a707b577e03f25e41f23fab4f676ebb8437e76b0
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1996-11-14 02:22:32 +0000

    missing `;'... not sure how I compiled it before

--- moira/server/startmoira.c
+++ moira/server/startmoira.c
@@ -1,7 +1,7 @@
 /*
  *	$Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/startmoira.c,v $
  *	$Author: danw $
- *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/startmoira.c,v 1.9 1996-09-29 20:06:56 danw Exp $
+ *	$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/startmoira.c,v 1.10 1996-11-14 02:22:32 danw Exp $
  *
  *	Copyright (C) 1987 by the Massachusetts Institute of Technology
  *	For copying and distribution information, please see the file
@@ -13,7 +13,7 @@
  */
 
 #ifndef lint
-static char *rcsid_mr_starter_c = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/startmoira.c,v 1.9 1996-09-29 20:06:56 danw Exp $";
+static char *rcsid_mr_starter_c = "$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/server/startmoira.c,v 1.10 1996-11-14 02:22:32 danw Exp $";
 #endif lint
 
 #include <mit-copyright.h>
@@ -27,7 +27,7 @@ static char *rcsid_mr_starter_c = "$Header: /afs/.athena.mit.edu/astaff/project/
 #include <moira_site.h>
 
 #define PROG	"moirad"
-char *whoami
+char *whoami;
 
 int rdpipe[2];
 extern char *sys_siglist[];

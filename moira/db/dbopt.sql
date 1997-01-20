3fe70db6c28fd0347b61e7d683129222cdb5b1c4
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1997-01-20 18:18:19 +0000

    Oracle changes (sqlplus prefers input files to have names ending in `.sql') newdb, newmoira, etc were out of date: New versions should be done before the public release.

---
+++ moira/db/dbopt.sql
@@ -0,0 +1,77 @@
+create unique index i_usr_usid on users (users_id);
+create index i_usr_login on users (login);
+create index i_usr_name on  users (first, last);
+create index i_usr_last on  users (last, first);
+create index i_usr_uid on  users (unix_uid);
+create index i_usr_pop on  users (pop_id);
+create index i_usr_clearid on users (clearid);
+
+create unique index i_krb_usid on krbmap  (users_id);
+create index i_krb_str on  krbmap (string_id);
+
+create unique index i_mac_mid on machine  (mach_id);
+create index i_mac_name on  machine (name);
+create index i_mac_addr on machine (address);
+create index i_mac_loc on machine (location);
+create index i_mac_snet on machine (snet_id);
+
+create unique index i_hal_name on hostalias  (name);
+create index i_hal_id on hostalias (mach_id);
+
+create unique index i_snt_sid on subnet  (snet_id);
+create index i_snt_name on subnet (name);
+create index i_snt_saddr on subnet(saddr);
+
+create unique index i_clu_clid on clusters (clu_id);
+create index i_clu_name on  clusters (name);
+
+create index i_mcm_clid on mcmap (clu_id);
+create index i_mcm_mach on  mcmap (mach_id);
+
+create index i_svc_clid on svc (clu_id);
+
+create index i_srv_name on servers (name);
+
+create index i_sho_srv on serverhosts (service);
+create index i_sho_mach on  serverhosts (mach_id);
+
+create unique index i_lis_lid on list  (list_id);
+create index i_lis_name on  list (name);
+create index i_lis_mail on  list (maillist);
+create index i_lis_group on  list (grouplist);
+create index i_lis_gid on  list (gid);
+create index i_lis_acl on list (acl_id);
+
+create index i_imem_mid on imembers (member_id, member_type);
+create index i_imem_list on  imembers (list_id);
+
+create unique index i_str_sid on strings  (string_id);
+create index i_str_name on  strings (string);
+
+create unique index i_nfs_nid on nfsphys  (nfsphys_id);
+create index i_nfs_mach on  nfsphys (mach_id);
+
+create unique index i_fil_fid on filesys  (filsys_id);
+create index i_fil_name on  filesys (label);
+create index i_fil_mach on  filesys (mach_id);
+create index i_fil_phys on  filesys (phys_id);
+create index i_fil_grp on  filesys (owners);
+create index i_fil_user on  filesys (owner);
+
+create index i_fsg_gid on fsgroup (group_id);
+create index i_fsg_filsys on  fsgroup (filsys_id);
+
+create index i_qot_eid on quota (entity_id);
+create index i_qot_fil on  quota (filsys_id);
+create index i_qot_phys on  quota (phys_id);
+
+create unique index i_zph_class on zephyr  (class);
+
+create unique index i_hac_mid on hostaccess  (mach_id);
+
+create index i_pcp_name on printcap (name);
+create index i_pcp_mach on  printcap (mach_id);
+
+create index i_plad_name on palladium (name);
+
+create index i_cap_tag on capacls (tag);

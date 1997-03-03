fcee862125b45a338dc0554e157bd8782bd4a5e9
tytso <tytso@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1997-03-03 19:32:24 +0000

    Add shell script for installing the warehouse data files.

---
+++ moira/gen/warehouse.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+#
+DIR=/warehouse/transfers/moirausr
+TAR_FILE=$DIR/moira.tar.Z
+
+PATH="/etc:/bin:/usr/bin:/usr/etc:/usr/athena/etc"
+
+# The following exit codes are defined and MUST BE CONSISTENT with the
+# MR error codes the library uses:
+set MR_MISSINGFILE = 	47836473
+set MR_TARERR = 	47836476
+
+cd $DIR
+
+echo extracting $TAR_FILE
+
+if [ ! -f $TAR_FILE ]; then
+	exit $MR_MISSINGFILE
+fi
+
+if zcat < $TAR_FILE | tar xvf -; then
+	echo Extract successful
+else
+	exit $MR_TARERR
+fi
+
+chmod 644 username_id.*
+
+rm -f $0
+echo removed self
+
+exit 0

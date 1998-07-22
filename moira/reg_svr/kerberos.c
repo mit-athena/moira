f3e01de70e95d3e3ce36b1ea29eda45c61ee9928
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1998-07-22 14:55:28 +0000

    New reg_svr

---
+++ moira/reg_svr/kerberos.c
@@ -0,0 +1,208 @@
+/* $Id: kerberos.c,v 1.1 1998-07-22 14:55:24 danw Exp $
+ *
+ * Kerberos routines for registration server
+ *
+ * Copyright (C) 1998 by the Massachusetts Institute of Technology
+ * For copying and distribution information, please see the file
+ * <mit-copyright.h>.
+ *
+ */
+
+#include <mit-copyright.h>
+#include <moira.h>
+#include "reg_svr.h"
+
+#if !defined(KRB4) && !defined(KRB5)
+#define KRB5
+#endif
+
+#include <errno.h>
+#include <string.h>
+
+#include <com_err.h>
+
+#ifdef KRB4
+#include <des.h>
+#include <kadm.h>
+#include <kadm_err.h>
+#include <krb.h>
+#endif
+
+#ifdef KRB5
+#include <kadm5/admin.h>
+#include <krb5.h>
+#include <krb.h>
+
+krb5_context context;
+#endif
+
+RCSID("$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/reg_svr/kerberos.c,v 1.1 1998-07-22 14:55:24 danw Exp $");
+
+extern char *hostname, *shorthostname;
+
+#ifdef KRB5
+long init_kerberos(void)
+{
+  krb5_error_code code;
+
+  /* Initialize Kerberos stuff. */
+  code = krb5_init_context(&context);
+  if (code)
+    return code;
+  krb_set_tkt_string("/tmp/tkt_ureg");
+  return 0;
+}
+
+/* Check the kerberos database to see if a principal exists */
+long check_kerberos(char *username)
+{
+  krb5_error_code code;
+  krb5_creds creds;
+  krb5_data *realm;
+  krb5_timestamp now;
+#ifdef KERBEROS_TEST_REALM
+  char ubuf[256];
+
+  sprintf(ubuf, "%s@%s", username, KERBEROS_TEST_REALM);
+  username = ubuf;
+#endif
+
+  memset(&creds, 0, sizeof(creds));
+  code = krb5_parse_name(context, username, &creds.client);
+  if (code)
+    goto cleanup;
+
+  realm = krb5_princ_realm(context, creds.client);
+  code = krb5_build_principal_ext(context, &creds.server,
+				  realm->length, realm->data,
+				  KRB5_TGS_NAME_SIZE, KRB5_TGS_NAME,
+				  realm->length, realm->data, 0);
+  if (code)
+    goto cleanup;
+
+  code = krb5_timeofday(context, &now);
+  if (code)
+    goto cleanup;
+
+  creds.times.starttime = 0;
+  creds.times.endtime = now + 60;
+
+  code = krb5_get_in_tkt_with_password(context,
+				       0    /* options */,
+				       NULL /* addrs */,
+				       NULL /* ktypes */,
+				       NULL /* pre_auth_types */,
+				       "x"  /* password */,
+				       NULL /* ccache */,
+				       &creds,
+				       NULL /* ret_as_reply */);
+
+cleanup:
+  krb5_free_principal(context, creds.client);
+  krb5_free_principal(context, creds.server);
+
+  if (code == KRB5KDC_ERR_C_PRINCIPAL_UNKNOWN)
+    return MR_SUCCESS;
+  else
+    return MR_IN_USE;
+}
+
+/* Create a new principal in Kerberos */
+long register_kerberos(char *username, char *password)
+{
+  void *kadm_server_handle = NULL;
+  kadm5_ret_t status;
+  kadm5_principal_ent_rec princ;
+  kadm5_config_params realm_params;
+  char admin_princ[256];
+#ifdef KERBEROS_TEST_REALM
+  char ubuf[256];
+
+  sprintf(admin_princ, "moira/%s@%s", hostname, KERBEROS_TEST_REALM);
+  sprintf(ubuf, "%s@%s", username, KERBEROS_TEST_REALM);
+  username = ubuf;
+  realm_params.realm = KERBEROS_TEST_REALM;
+  realm_params.mask = KADM5_CONFIG_REALM;
+#else
+  sprintf(admin_princ, "moira/%s", hostname);
+  realm_params.mask = 0;
+#endif
+
+  status = krb5_parse_name(context, username, &(princ.principal));
+  if (status)
+    return status;
+
+
+  status = kadm5_init_with_skey(admin_princ, NULL, KADM5_ADMIN_SERVICE,
+				&realm_params, KADM5_STRUCT_VERSION,
+				KADM5_API_VERSION_2, &kadm_server_handle);
+  if (status)
+    goto cleanup;
+
+  status = kadm5_create_principal(kadm_server_handle, &princ,
+				  KADM5_PRINCIPAL, password);
+
+cleanup:
+  krb5_free_principal(context, princ.principal);
+  if (kadm_server_handle)
+    kadm5_destroy(kadm_server_handle);
+
+  if (status == KADM5_DUP)
+    return MR_IN_USE;
+  else return status;
+}
+#endif
+
+#ifdef KRB4
+char realm[REALM_SZ];
+
+long init_kerberos(void)
+{
+  return krb_get_lrealm(realm, 1);
+}
+
+long check_kerberos(char *username)
+{
+  long status;
+
+  status = krb_get_pw_in_tkt(username, "", realm, "krbtgt", realm, 1, "");
+  if (status == KDC_PR_UNKNOWN)
+    return MR_SUCCESS;
+  else
+    return MR_IN_USE;
+}
+
+long register_kerberos(char *username, char *password)
+{
+  long status;
+  Kadm_vals new;
+  des_cblock key;
+  unsigned long *lkey = (unsigned long *)key;
+
+  if ((status = krb_get_svc_in_tkt(MOIRA_SNAME, shorthostname, realm,
+				   PWSERV_NAME, KADM_SINST, 1, KEYFILE)))
+    return status;
+
+  if ((status = kadm_init_link(PWSERV_NAME, KADM_SINST, realm)) !=
+      KADM_SUCCESS)
+    return status;
+
+  memset(&new, 0, sizeof(new));
+  SET_FIELD(KADM_DESKEY, new.fields);
+  SET_FIELD(KADM_NAME, new.fields);
+
+  des_string_to_key(password, key);
+  new.key_low = htonl(lkey[0]);
+  new.key_high = htonl(lkey[1]);
+  strcpy(new.name, username);
+
+  status = kadm_add(&new);
+  memset(&new, 0, sizeof(new));
+  dest_tkt();
+
+  if (status == KADM_INUSE)
+    return MR_IN_USE;
+  else
+    return status;
+}
+#endif
--- moira/reg_svr/reg_svr.h
+++ moira/reg_svr/reg_svr.h
@@ -1,77 +1,77 @@
-/*
- *      $Source: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/reg_svr/reg_svr.h,v $
- *      $Author: danw $
- *      $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/reg_svr/reg_svr.h,v 1.11 1998-02-05 22:51:34 danw Exp $
- *
- *      Copyright (C) 1987 by the Massachusetts Institute of Technology
- *
- *      Server for user registration with Moira and Kerberos.
- *
- *      This file contains all the information needed by all source
- *      files for the user registration server.
- */
+#include <sys/time.h>
+#include <des.h>
 
-#include <sys/types.h>
-#include <ctype.h>
-#include "ureg_err.h"
-#define REG_SVR
-#include "ureg_proto.h"
+typedef struct reg_client {
+  int fd;				/* socket */
+  int clientid;				/* client id */
+  unsigned char *buf;			/* buffer for data read */
+  int nread, nmax;			/* # of bytes cur/max in buffer */
+  time_t lastmod;			/* last time we sent/received */
+  int encrypted;			/* set if sched is valid */
+  des_key_schedule sched;		/* DES key schedule */
+  long uid;				/* uid of user. set by RIFO */
+  char *id;				/* stored MIT ID if "six words" are
+					   needed. NULL otherwise. Set by
+					   RIFO, cleared by SWRD */
+  char *suggestions;			/* suggested usernames */
+  char *username;			/* desired username. set by LOGN */
+  int reserved_username;		/* if username wasn't picked by user */
+  unsigned short *random;		/* random state */
+} reg_client;
 
-#ifndef TRUE
-#define TRUE 1
-#endif
+void RIFO(reg_client *rc, int argc, char **argv);
+void SWRD(reg_client *rc, int argc, char **argv);
+void LOGN(reg_client *rc, int argc, char **argv);
+void PSWD(reg_client *rc, int argc, char **argv);
+void QUIT(reg_client *rc, int argc, char **argv);
 
-#ifndef FALSE
-#define FALSE 0
-#endif
+#define REG_SVR_PRINCIPAL "sms"
+#define REG_SVR_INSTANCE ""
 
-#define FAIL_INST "reg_svr"	/* Instance for failure zephyrgrams */
+#define REG_SVR_RSA_KEY "/moira/reg_svr/rsa_key"
+#define REG_SVR_HMAC_KEY "/moira/reg_svr/hmac_key"
 
-#define CUR_UREG_VERSION 1	/* Version for the register protocol */
-#define SUCCESS 0		/* General purpose success code */
-#define FAILURE 1		/* To use when any non-zero number will work */
-#define min(a,b) ((a) > (b) ? (b) : (a))
-#define MIN_UNAME 3		/* Username must be between three and */
-#define MAX_UNAME 8		/*    eight characters long. */
-#define CRYPT_LEN 14		/* crypt() returns a 13 char string */
-#define LOGIN_LEN MAX_UNAME + 1	/* Leave room for a null */
-#define UID_LEN 7		/* Allow room for a 16 bit number */
+#define REG_SVR_ERROR_MESSAGES "/moira/reg_svr/errors"
+enum { NO_MESSAGE, INTERNAL_ERROR, PROTOCOL_ERROR, DATABASE_CLOSED,
+       ENCRYPT_KEY, NOT_FOUND_IN_DATABASE, ALREADY_REGISTERED,
+       ACCOUNT_DELETED, NOT_ELIGIBLE, FOUND, FORCED_USERNAME,
+       BAD_SIX_WORDS, BAD_USERNAME, USERNAME_UNAVAILABLE, 
+       RESERVED_USERNAME_UNAVAILABLE, USERNAME_OK, PASSWORD_SHORT,
+       PASSWORD_SIMPLE, PASSWORD_SAMPLE, KADM_ERROR, DONE,
+       NUM_REG_ERRORS };
 
-extern char *whoami;		/* Name of program - used by libraries */
+#define TIMEOUT 300 /* 5 minutes */
 
-/* This structure holds information from the Moira database that will be
-   worth holding on to.  An instance of it appears in the formatted
-   packet structure. */
-struct db_data
-{
-  char mit_id[CRYPT_LEN];	/* Encrypted MIT ID */
-  int reg_status;		/* Registration status */
-  char uid[UID_LEN];		/* Reserved uid */
-  char login[LOGIN_LEN];	/* Login (username) */
-};
+#define REG_RSA_ENCRYPTED_KEY 0x43
+#define REG_ENCRYPTED 0x45
+#define REG_UNENCRYPTED 0x50
 
-/* This structure stores information sent over in the packet in a
-   more convenient format and also stores some information obtained
-   from the database that will be needed for each transaction.  It
-   initialized from format_pkt() and find_user(). */
-struct msg
-{
-  U_32BIT version;		/* User registration protocol version */
-  U_32BIT request;		/* Request */
-  char *first;			/* First name */
-  char *last;			/* Last name */
-  char *encrypted;		/* Encrypted information in packet */
-  int encrypted_len;		/* Length of encrypted information in packet */
-  char *leftover;		/* Leftover information sent in the packet */
-  int leftover_len;		/* Length of leftover information */
-  struct db_data db;		/* Information from the Moira database */
-};
+/* Prototypes from kerberos.c */
+long init_kerberos(void);
+long check_kerberos(char *username);
+long register_kerberos(char *username, char *password);
 
-/* For logging successful database transactions */
-extern FILE *journal;
+/* Prototypes from procotol.c */
+int read_rsa_key(void);
+int read_errors(void);
+void reply(reg_client *rc, int msg, char *state, char *clean, char *data,
+	   ...);
+void parse_packet(reg_client *rc, int type, long len, char *buf, int sleeping);
 
-/* prototypes from requests.c */
-void req_initialize(void);
-void get_request(struct msg *message);
-void report(int status, char *message);
-u_long cur_req_sender(void);
+/* prototypes from reg_svr.pc */
+void *xmalloc(size_t);
+void *xrealloc(void *, size_t);
+char *xstrdup(char *);
+
+/* Prototypes from words.c */
+int read_hmac_key(void);
+unsigned short *init_rand(void *);
+void getwordlist(char *input, char *words[]);
+
+
+/* XXX Stuff for testing */
+#define KERBEROS_TEST_REALM "ZONE.MIT.EDU"
+#undef REG_SVR_PRINCIPAL
+#define REG_SVR_PRINCIPAL "rcmd"
+#undef REG_SVR_INSTANCE
+#define REG_SVR_INSTANCE "this-too-shall-pass"

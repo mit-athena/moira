92ea5fb1ecf84a5d4ded46b1df4ba1af4fe5aa81
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1999-01-27 19:38:40 +0000

    changes for tar.c

--- moira/gen/util.h
+++ moira/gen/util.h
@@ -3,8 +3,46 @@
 /* for MAXPATHLEN */
 #include <sys/param.h>
 
+#include <stdio.h>
+
 void fix_file(char *targetfile);
 char *dequote(char *s);
 void db_error(int code);
 
 int ModDiff(int *flag, char *tbl, time_t ModTime);
+time_t unixtime(char *timestring);
+#define UNIXTIME_FMT "J HH24 MI SS"
+
+struct tarheader {
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char chksum[8];
+  char typeflag[1];
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+  char pad[12];
+};
+
+typedef struct _tarfile {
+  FILE *fp;
+  struct tarheader th;
+  long offset;
+} TARFILE;
+
+TARFILE *tarfile_open(char *file);
+void tarfile_close(TARFILE *tf);
+FILE *tarfile_start(TARFILE *tf, char *name, mode_t mode, uid_t uid, gid_t gid,
+		    char *user, char *group, time_t mtime);
+void tarfile_end(TARFILE *tf);
+void tarfile_mkdir(TARFILE *tf, char *name, mode_t mode, uid_t uid, gid_t gid,
+		   char *user, char *group, time_t mtime);

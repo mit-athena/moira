7568531047f03e7f46eee0401629c9ef90839669
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1999-03-30 04:02:03 +0000

    various fixes from the copy on moira

--- moira/gen/boot.sh
+++ moira/gen/boot.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $Id: boot.sh,v 1.1 1999-01-27 19:39:22 danw Exp $
+# $Id: boot.sh,v 1.2 1999-03-30 04:02:03 danw Exp $
 
 # The following exit codes are defined and MUST BE CONSISTENT with the
 # error codes the library uses:
@@ -7,7 +7,7 @@ MR_MISSINGFILE=47836473
 MR_MKCRED=47836474
 MR_TARERR=47836476
 
-PATH=...
+PATH=/usr/bin
 TARFILE=/var/tmp/boot.out
 BOOTGEN=/var/boot/bootptab.print
 BOOTLOCAL=/var/boot/bootptab.local
@@ -19,25 +19,17 @@ test -r $TARFILE || exit $MR_MISSINGFILE
 test -r $BOOTLOCAL || exit $MR_MISSINGFILE
 test -r $PSWDFILE || exit $MR_MISSINGFILE
 
-# Unpack the tar file, getting only files that are newer than the
-# on-disk copies (-u). When extracting files under /var/boot/hp,
-# tack on a .new so we can recognize them in the next step
 cd /
-pax -ru -s '|/var/boot/hp/.*$/&.new/' -f $TARFILE || exit $MR_TARERR
+tar xf $TARFILE || exit $MR_TARERR
 
 # Append passwords, etc., to the new files
-for f in find /var/boot/hp -name \*.new -print; do
+for f in `find /var/boot/hp -name \*.new -print`; do
     cat $PSWDFILE >> $f
     mv $f /var/boot/hp/`basename $f .new`
 done
 
 # Build full bootptab
 cat $BOOTLOCAL $BOOTGEN > $BOOTFILE
-
-# Signal bootpd? XXX
-
-# Remove out-of-date hp files
-find /var/boot/hp -mtime +1 -exec rm -f {} \;
 
 # cleanup
 test -f $TARFILE && rm -f $TARFILE

d757a7147c697ac4fe1fa15e5398739a10783225
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1999-05-13 18:55:17 +0000

    use libmrclient to connect, get ticket file principal name, and sanity check smtp poboxes

--- moira/clients/passwd/chpobox.c
+++ moira/clients/passwd/chpobox.c
@@ -1,4 +1,4 @@
-/* $Id: chpobox.c,v 1.23 1999-04-30 17:39:38 danw Exp $
+/* $Id: chpobox.c,v 1.24 1999-05-13 18:55:17 danw Exp $
  *
  * Talk to the Moira database to change a person's home mail machine. This may
  * be an Athena machine, or a completely arbitrary address.
@@ -22,6 +22,7 @@
 #include <mit-copyright.h>
 #include <moira.h>
 #include <moira_site.h>
+#include <mrclient.h>
 
 #include <pwd.h>
 #include <stdio.h>
@@ -29,14 +30,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <krb.h>
-
-RCSID("$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/passwd/chpobox.c,v 1.23 1999-04-30 17:39:38 danw Exp $");
+RCSID("$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/clients/passwd/chpobox.c,v 1.24 1999-05-13 18:55:17 danw Exp $");
 
 int get_pobox(int argc, char **argv, void *callarg);
-char *potype(char *machine);
-int check_match(int argc, char **argv, void *callback);
-int check_match3(int argc, char **argv, void *callback);
 void usage(void);
 
 char *whoami;
@@ -46,17 +42,12 @@ static int match;
 int main(int argc, char *argv[])
 {
   struct passwd *pwd;
-  char *mrarg[3], buf[BUFSIZ], pname[ANAME_SZ];
-  char *address, *uname, *machine, *motd;
-  uid_t u;
-  int c, setflag, prevpop, usageflag, status;
+  char *mrarg[3];
+  char *address, *uname;
+  int c, setflag, prevpop, status;
 
-  extern int optind;
-  extern char *optarg;
-
-  c = setflag = prevpop = usageflag = 0;
+  c = setflag = prevpop = 0;
   address = uname = NULL;
-  u = getuid();
 
   if ((whoami = strrchr(argv[0], '/')) == NULL)
     whoami = argv[0];
@@ -71,154 +62,61 @@ int main(int argc, char *argv[])
       {
       case 's':
 	if (prevpop)
-	  usageflag++;
+	  usage();
 	else
 	  {
 	    setflag++;
-	    strcpy(buf, optarg);
-	    address = buf;
+	    address = optarg;
 	  }
 	break;
       case 'p':
 	if (setflag)
-	  usageflag++;
+	  usage();
 	else
 	  prevpop++;
 	break;
       case 'u':
-	uname = strdup(optarg);
+	uname = optarg;
 	break;
       default:
-	usageflag++;
+	usage();
 	break;
       }
   if (argc == 2 && optind == 1 && !uname)
     uname = argv[optind++];
 
-  if (usageflag || optind != argc)
+  if (optind != argc)
     usage();
 
   if (!uname)
     {
-      if ((status = tf_init(TKT_FILE, R_TKT_FIL)) ||
-	  (status = tf_get_pname(pname)))
-	{
-	  com_err(whoami, status, "reading Kerberos ticket file");
-	  exit(1);
-	}
-      tf_close();
-      uname = pname;
+      uname = mrcl_krb_user();
+      if (!uname)
+	exit(1);
     }
   mrarg[0] = uname;
 
-  status = mr_connect(NULL);
-  if (status)
-    {
-      com_err(whoami, status, " while connecting to Moira");
-      exit(1);
-    }
-
-  status = mr_motd(&motd);
-  if (status)
-    {
-      mr_disconnect();
-      com_err(whoami, status, " unable to check server status");
-      exit(1);
-    }
-  if (motd)
-    {
-      fprintf(stderr, "The Moira server is currently unavailable:\n%s\n",
-	      motd);
-      mr_disconnect();
-      exit(1);
-    }
-
-  status = mr_auth("chpobox");
-  if (status)
-    {
-      com_err(whoami, status, " while authenticating -- "
-	      "run \"kinit\" and try again.");
-      mr_disconnect();
-      exit(1);
-    }
+  if (mrcl_connect(NULL, "chpobox", 1) != MRCL_SUCCESS)
+    exit(1);
 
   if (setflag)
     {
-      /* Address is of form user@host.  Split it up. */
-      if (!address)
+      char *addr;
+      if (mrcl_validate_pobox_smtp(uname, address, &addr) != MRCL_SUCCESS)
 	{
-	  fprintf(stderr, "%s: no address was specified.\n", whoami);
+	  printf("\n");
 	  goto show;
 	}
-      machine = strchr(address, '@');
-      if (machine)
-	{
-	  *machine++ = '\0';		/* get rid of the @ sign */
-	  machine = strtrim(machine);	/* get rid of whitespace */
-	}
-      else
-	{
-	  fprintf(stderr, "%s: no at sign (@) in address \"%s\"\n",
-		  whoami, address);
-	  goto show;
-	}
-      mrarg[2] = canonicalize_hostname(strdup(machine));
-      mrarg[1] = potype(mrarg[2]);
-      if (!strcmp(mrarg[1], "POP"))
-	{
-	  if (strcmp(address, uname))
-	    {
-	      fprintf(stderr,
-		      "%s: the name on the POP box must match the username\n",
-		      whoami);
-	      goto show;
-	    }
-	}
-      else if (!strcmp(mrarg[1], "LOCAL"))
-	{
-	  strcat(address, "@");
-	  strcat(address, mrarg[2]);
-	  mrarg[2] = address;
-	  if ((address = strchr(address, '@')) &&
-	      (address = strchr(address, '.')))
-	    *address = 0;
-	  strcat(mrarg[2], ".LOCAL");
-	  mrarg[1] = "SMTP";
-	}
-      else if (!strcmp(mrarg[1], "MAILHUB"))
-	{
-	  if (!strcmp(address, uname))
-	    {
-	      fprintf(stderr,
-		      "Error: this will set a mail forwarding loop.\n");
-	      fprintf(stderr,
-		      "Use \"%s -p\" to set a local post office server.\n",
-		      whoami);
-	      exit(1);
-	    }
-	  fprintf(stderr, "Error: \"%s@%s\" is a local mail address.\n",
-		  address, machine);
-	  fprintf(stderr, "Your mail drop must be on a post office server "
-		  "or an external mail address.\n");
-	  exit(1);
-	}
-      else if (mrarg[1])
-	{
-	  if (*machine != '"' && strcasecmp(mrarg[2], machine))
-	    {
-	      fprintf(stderr, "Warning: hostname %s canonicalized to %s\n",
-		      machine, mrarg[2]);
-	    }
-	  strcat(address, "@");
-	  strcat(address, mrarg[2]);
-	  mrarg[2] = address;
-	} else
-	  goto show;
+      mrarg[1] = "SMTP";
+      mrarg[2] = addr;
       status = mr_query("set_pobox", 3, mrarg, NULL, NULL);
+      free(addr);
       if (status)
-	com_err(whoami, status,
-		" while setting pobox for %s to type %s, box %s",
-		mrarg[0], mrarg[1], mrarg[2]);
+	{
+	  com_err(whoami, status,
+		  "while setting pobox for %s to type %s, box %s",
+		  mrarg[0], mrarg[1], mrarg[2]);
+	}
     }
   else if (prevpop)
     {
@@ -259,73 +157,6 @@ int get_pobox(int argc, char **argv, void *callarg)
     printf("User %s, Type %s, Box: %s\n",
 	   argv[0], argv[1], argv[2]);
   printf("  Modified by %s on %s with %s\n", argv[4], argv[3], argv[5]);
-  return 0;
-}
-
-/*
- * given a canonicalized machine name, ask the Moira server if it is of type
- * pop, or of type local -- if neither, we assume that it's of type foreign.
- */
-char *potype(char *machine)
-{
-  char *service[1], *argv[3];
-  int status;
-
-  match = 0;
-  service[0] = "POP";
-  status = mr_query("get_server_locations", 1, service, check_match, machine);
-  if (status && (status != MR_NO_MATCH))
-    {
-      com_err(whoami, status, " while reading list of POP servers");
-      return NULL;
-    }
-  if (match)
-    return "POP";
-
-  service[0] = "LOCAL";
-  status = mr_query("get_server_locations", 1, service, check_match, machine);
-  if (status && (status != MR_NO_MATCH))
-    {
-      com_err(whoami, status, " while reading list of LOCAL servers");
-      return NULL;
-    }
-  if (match)
-    return "LOCAL";
-
-  argv[0] = "mailhub";
-  argv[1] = "TYPE";
-  argv[2] = "*";
-  status = mr_query("get_alias", 3, argv, check_match3, machine);
-  if (status && (status != MR_NO_MATCH))
-    {
-      com_err(whoami, status, " while reading list of MAILHUB servers");
-      return NULL;
-    }
-  if (match)
-    return "MAILHUB";
-  else
-    return "SMTP";
-}
-
-int check_match(int argc, char **argv, void *callback)
-{
-  if (match)
-    return 0;
-
-  if (!strcasecmp(argv[1], callback))
-    match = 1;
-
-  return 0;
-}
-
-int check_match3(int argc, char **argv, void *callback)
-{
-  if (match)
-    return 0;
-
-  if (!strcasecmp(argv[2], callback))
-    match = 1;
-
   return 0;
 }
 

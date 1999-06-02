399f202fe894f34ce83417e7a4e956aa9db8306a
danw <danw@1760404a-f4a1-11de-aea4-874f16f9dcc5>
1999-06-02 22:23:03 +0000

    printng.gen, the print DCM for LPRng servers. Will be merged back into print.gen after the old print servers go away

---
+++ moira/gen/printng.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+# $Id: printng.sh,v 1.1 1999-06-02 22:23:03 danw Exp $
+
+# The following exit codes are defined and MUST BE CONSISTENT with the
+# error codes the library uses:
+MR_MISSINGFILE=47836473
+MR_MKCRED=47836474
+MR_TARERR=47836476
+
+PATH=/bin
+TARFILE=/var/tmp/print.out
+PCLOCAL=/etc/printcap.local
+PCGEN=/etc/printcap.moira
+PRINTCAP=/etc/printcap
+
+# Alert if the tar file or other needed files do not exist
+test -r $TARFILE || exit $MR_MISSINGFILE
+test -r $PCLOCAL || exit $MR_MISSINGFILE
+
+# Unpack the tar file, getting only files that are newer than the
+# on-disk copies (-u).
+cd /
+tar xf $TARFILE || exit $MR_TARERR
+
+# Build full printcap and spools
+cat $PCLOCAL $PCGEN > $PRINTCAP
+/usr/athena/bin/checkpc -f
+
+# cleanup
+test -f $TARFILE && rm -f $TARFILE
+test -f $0 && rm -f $0
+
+exit 0
+

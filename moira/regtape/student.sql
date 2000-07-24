1e72d3e9460faa92b4b65a689b19ac62931807ff
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2000-07-24 05:35:54 +0000

    Change location of spool file to /moira/load, to agree with staff.sql.

--- moira/regtape/student.sql
+++ moira/regtape/student.sql
@@ -2,7 +2,7 @@ set head off
 set feedback off
 set pagesize 0
 set linesize 256
-spool /var/tmp/student.input.unsorted
+spool /moira/load/student.input.unsorted
 select rpad(nvl(mit_id, ' '), 9, ' ') || rpad(nvl(last_name, ' '), 30, ' ') || rpad(nvl(first_name, ' '), 30, ' ') || rpad(nvl(middle_name, ' '), 30, ' ') || rpad(nvl(student_year, ' '), 1, ' ') || rpad(nvl(department, ' '), 4, ' ') || rpad(nvl(term_street1, ' '), 30, ' ') || rpad(nvl(term_city, ' '), 40, ' ') || rpad(nvl(term_state, ' '), 2, ' ') || rpad(nvl(term_zip, ' '), 9, ' ') || rpad(nvl(term_phone1, ' '), 20, ' ') || rpad(nvl(office_location, ' '), 30, ' ') || rpad(nvl(office_phone, ' '), 20, ' ') from wareuser.moira_student3;
 spool off
 quit

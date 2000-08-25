4d482a87854b0024c304a47950f9620dff0baf1f
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2000-08-25 23:06:36 +0000

    Add MRCL_WARN return code.

--- moira/include/mrclient.h
+++ moira/include/mrclient.h
@@ -1,4 +1,4 @@
-/* $Id: mrclient.h,v 1.3 2000-03-29 20:49:35 zacheiss Exp $
+/* $Id: mrclient.h,v 1.4 2000-08-25 23:06:36 zacheiss Exp $
  *
  * Copyright (C) 1999 by the Massachusetts Institute of Technology
  *
@@ -10,9 +10,10 @@
 #define MRCL_SUCCESS		0
 #define MRCL_FAIL		1
 #define MRCL_REJECT		2
-#define MRCL_ENOMEM		3
-#define MRCL_MOIRA_ERROR	4
-#define MRCL_AUTH_ERROR		4
+#define MRCL_WARN               3
+#define MRCL_ENOMEM		4
+#define MRCL_MOIRA_ERROR	5
+#define MRCL_AUTH_ERROR		6
 
 int mrcl_connect(char *server, char *client, int version, int auth);
 char *mrcl_krb_user(void);

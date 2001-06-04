410b515c9d363b8ebc3cf38f04ad1156fc646e26
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2001-06-04 21:07:49 +0000

    nmailhub service is now mailhub service.

--- moira/gen/mailhub.sh
+++ moira/gen/mailhub.sh
@@ -1,44 +1,38 @@
 #!/bin/sh
 PATH=/bin:/usr/ucb:/usr/bin
-
-if [ -d /var/athena ] && [ -w /var/athena ]; then
-    exec >/var/athena/moira_update.log 2>&1
-else
-    exec >/tmp/moira_update.log 2>&1
-fi
+root=/usr/local/sendmail
 
 MR_MKCRED=47836474
 
-cat /usr/lib/aliases.legacy > /usr/lib/aliases.tmp
-cat /usr/lib/aliases.new >> /usr/lib/aliases.tmp
-cat /usr/lib/aliases.local >> /usr/lib/aliases.tmp
-mv aliases.tmp aliases.new
+cat $root/etc/aliases.legacy > $root/etc/aliases.tmp
+cat $root/etc/aliases.new >> $root/etc/aliases.tmp
+cat $root/etc/aliases.local >> $root/etc/aliases.tmp
+mv $root/etc/aliases.tmp $root/etc/aliases.new
 
-cp /dev/null /usr/lib/aliases.new.dir
-cp /dev/null /usr/lib/aliases.new.pag
+cp /dev/null $root/etc/aliases.new.db
 
-/usr/lib/sendmail -bi -oA/usr/lib/aliases.new
+$root/sbin/sendmail -bi -oA$root/etc/aliases.new
 if [ $? != 0 ]; then
 	exit $MR_MKCRED
-	fi
+fi
 
-kill `ps ax | grep "sendmail" | egrep -v "grep|mqueue.stall" | awk '{print $1}'`
+kill `ps -el | grep "sendmail" | egrep -v "grep|mqueue.stall" | awk '{print $4}'`
 sleep 60
 
-mv /usr/lib/aliases /usr/lib/aliases.old
-mv /usr/lib/aliases.dir /usr/lib/aliases.old.dir
-mv /usr/lib/aliases.pag /usr/lib/aliases.old.pag
-mv /usr/lib/aliases.new /usr/lib/aliases
-mv /usr/lib/aliases.new.dir /usr/lib/aliases.dir
-mv /usr/lib/aliases.new.pag /usr/lib/aliases.pag
+mv $root/etc/aliases $root/etc/aliases.old
+mv $root/etc/aliases.db $root/etc/aliases.old.db
+mv $root/etc/aliases.new $root/etc/aliases
+mv $root/etc/aliases.new.db $root/etc/aliases.db
 
 cd /usr/spool/mqueue
 rm -f xf* tf* lf* nf*
 
-/usr/lib/sendmail -bd
-/usr/lib/sendmail -q20m
+#$root/sbin/sendmail -bd
+#$root/sbin/sendmail -q20m
+
+sh /etc/init.d/sendmail start
 
 rm -f $0
 exit 0
 
-# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/mailhub.sh,v 1.8 2000-05-08 18:30:30 zacheiss Exp $
+# $Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/gen/mailhub.sh,v 1.9 2001-06-04 21:07:49 zacheiss Exp $

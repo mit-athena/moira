03def4229820bcfca23e9b7400403f12bece45d7
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2002-09-16 22:32:10 +0000

    Correct table name.

--- moira/regtape/staff.sql
+++ moira/regtape/staff.sql
@@ -3,6 +3,6 @@ set feedback off
 set pagesize 0
 set linesize 271
 spool /moira/load/staff.input.unsorted
-select mit_id || rpad(nvl(full_name, ' '), 30, ' ') || rpad(substr(nvl(office_location, ' '), 0, 12), 12, ' ') || rpad(substr(nvl(secondary_office_location, ' '), 0, 12), 12, ' ') || lpad(substr(nvl(office_phone, ' '), 0, 12), 12, ' ') || lpad(substr(nvl(secondary_office_phone, ' '), 0, 12), 12, ' ') || rpad(substr(department_name, 0, 50), 50, ' ') || substr(directory_title, 0, 50) from wareuser.moira_employee2;
+select mit_id || rpad(nvl(full_name, ' '), 30, ' ') || rpad(substr(nvl(office_location, ' '), 0, 12), 12, ' ') || rpad(substr(nvl(secondary_office_location, ' '), 0, 12), 12, ' ') || lpad(substr(nvl(office_phone, ' '), 0, 12), 12, ' ') || lpad(substr(nvl(secondary_office_phone, ' '), 0, 12), 12, ' ') || rpad(substr(department_name, 0, 50), 50, ' ') || substr(directory_title, 0, 50) from wareuser.moira_employee;
 spool off
 quit

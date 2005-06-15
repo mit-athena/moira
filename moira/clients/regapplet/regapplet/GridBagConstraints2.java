261d064440edd5b3bbefbdf65a2f752c23beadef
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2005-06-15 09:01:47 +0000

    "public synchronized class" isn't allowed by the Java spec, and modern JDKs enforce it.  Not clear it ever did anything anyway.

--- moira/clients/regapplet/regapplet/GridBagConstraints2.java
+++ moira/clients/regapplet/regapplet/GridBagConstraints2.java
@@ -3,7 +3,7 @@ package regapplet;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
-public synchronized class GridBagConstraints2 extends GridBagConstraints
+public class GridBagConstraints2 extends GridBagConstraints
 {
     public GridBagConstraints2(int i1, int j1, int k1, int i2, double d1, double d2, int j2, int k2, Insets insets, int i3, int j3)
     {

98ace237c1a311773f31da1052e7130f4eefa284
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2010-01-04 22:02:24 +0000

    Install and look for moira.conf in sysconfdir, wherever that happens to be.

--- moira/update/config.c
+++ moira/update/config.c
@@ -26,8 +26,6 @@
 
 RCSID("$Header: /afs/.athena.mit.edu/astaff/project/moiradev/repository/moira/update/config.c,v 1.7 1998-02-15 17:49:27 danw Exp $");
 
-#define CONFIG_FILE	"/etc/athena/moira.conf"
-
 /* Variables currently supported:
  * chroot directory	daemon will run chrooted to this directory
  * user username	daemon will run with this user's uid

710bbd025dce0b8a194ed4f47401da5c41cb1a71
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2010-01-04 17:57:37 +0000

    From broder: Fix OS X com_err whackiness.

--- moira/clients/mrtest/mrtest.c
+++ moira/clients/mrtest/mrtest.c
@@ -97,8 +97,13 @@ int main(int argc, char **argv)
 	}
     }
 
+#if defined(__APPLE__) && defined(__MACH__)
+  add_error_table(&et_sms_error_table);
+  add_error_table(&et_krb_error_table);
+#else
   initialize_sms_error_table();
   initialize_krb_error_table();
+#endif
 
 #ifdef HAVE_READLINE
   /* we don't want filename completion */
--- moira/lib/mr_init.c
+++ moira/lib/mr_init.c
@@ -20,8 +20,15 @@ void mr_init(void)
   if (mr_inited)
     return;
 
+#if defined(__APPLE__) && defined(__MACH__)
+  add_error_table(&et_sms_error_table);
+  add_error_table(&et_krb_error_table);
+  add_error_table(&et_ureg_error_table);
+#else
   initialize_sms_error_table();
   initialize_krb_error_table();
   initialize_ureg_error_table();
+#endif
+
   mr_inited = 1;
 }

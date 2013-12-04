590add53e129285f63e951417feebac875332c0a
zacheiss <zacheiss@1760404a-f4a1-11de-aea4-874f16f9dcc5>
2013-12-04 01:45:08 +0000

    Make sure MR_STMT_BUF is larger than MAX_FIELD_WIDTH.

--- moira/server/qrtn.h
+++ moira/server/qrtn.h
@@ -3,7 +3,7 @@
  *  Used by the SQL query routines of the Moira server.
  */
 
-#define MR_STMTBUF_LEN 1024
+#define MR_STMTBUF_LEN (MAX_FIELD_WIDTH * 2)
 extern char stmt_buf[MR_STMTBUF_LEN];
 
 void dbmserr(void);

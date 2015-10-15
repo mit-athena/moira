d89726a673da97477a0d83055996d237938b7d02
Garry Zacheiss <zacheiss@mit.edu>
2015-10-15 18:22:15 -0400

    Pick up six digit department codes, not just names.

--- moira/regtape/student.sql
+++ moira/regtape/student.sql
@@ -3,6 +3,6 @@ set feedback off
 set pagesize 0
 set linesize 512
 spool /moira/load/student.input.unsorted
-select rpad(nvl(mit_id, ' '), 9, ' ') || rpad(nvl(last_name, ' '), 30, ' ') || rpad(nvl(first_name, ' '), 30, ' ') || rpad(nvl(middle_name, ' '), 30, ' ') || rpad(nvl(student_year, ' '), 1, ' ') || rpad(nvl(department, ' '), 4, ' ') || rpad(nvl(term_street1, ' '), 30, ' ') || rpad(nvl(term_city, ' '), 40, ' ') || rpad(nvl(term_state, ' '), 2, ' ') || rpad(nvl(term_zip, ' '), 9, ' ') || rpad(nvl(term_phone1, ' '), 20, ' ') || rpad(nvl(office_location, ' '), 30, ' ') || rpad(nvl(office_phone, ' '), 20, ' ') || rpad(nvl(reg_type, ' '), 50, ' ') || rpad(nvl(lower(email_address), ' '), 60, ' ') from wareuser.moira_student;
+select rpad(nvl(mit_id, ' '), 9, ' ') || rpad(nvl(last_name, ' '), 30, ' ') || rpad(nvl(first_name, ' '), 30, ' ') || rpad(nvl(middle_name, ' '), 30, ' ') || rpad(nvl(student_year, ' '), 1, ' ') || rpad(nvl(department, ' '), 4, ' ') || rpad(nvl(term_street1, ' '), 30, ' ') || rpad(nvl(term_city, ' '), 40, ' ') || rpad(nvl(term_state, ' '), 2, ' ') || rpad(nvl(term_zip, ' '), 9, ' ') || rpad(nvl(term_phone1, ' '), 20, ' ') || rpad(nvl(office_location, ' '), 30, ' ') || rpad(nvl(office_phone, ' '), 20, ' ') || rpad(nvl(reg_type, ' '), 50, ' ') || rpad(nvl(lower(email_address), ' '), 60, ' ') || rpad(nvl(department_number, '0'), 6, ' ') from wareuser.moira_student;
 spool off
 quit
